942ddb7efcbd688a182f9d0bcbd5ae8b48295b95
Saravanan <sarav.majestic@gmail.com>
2023-09-27 07:43:05 +0000

    remove extra mode field

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -7,7 +7,6 @@ function App() {
     const headers: HeadersInit = {
       "Access-Control-Allow-Origin": "*",
       "Content-Type": "application/json",
-      mode: "cors",
     };
 
     const response = await fetch(
